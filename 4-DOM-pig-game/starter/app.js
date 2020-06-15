e2b845c08ff1d87aa47f64ce20b9851f969f988a
OxiCuza <oxicusa@gmail.com>
2020-06-15 21:01:48 +0700

    set n get value dice with querySelector

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,3 +15,6 @@ roundScore = 0;
 scores = []
 activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
