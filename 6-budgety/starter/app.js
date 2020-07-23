8facc4b1d1c3ab75d857cd3e960629b86ab3fe54
OxiCuza <oxicusa@gmail.com>
2020-07-23 11:52:20 +0700

    reading input data n create init function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,32 +1,52 @@
 // BUDGET CONTROLLER
-let budgetController = (function () {
-    
-
-})();
+let BudgetController = (function () {})();
 
 // UI CONTROLLER
-let uiController = (function () {
-    
+let UIController = (function () {
+	let DOMStr = {
+		inputType: "add__type",
+		inputDesc: "add__description",
+		inputValue: "add__value",
+		inputBtn: "add_btn",
+	};
 
+	return {
+		getInput: function () {
+			return {
+				type: document.querySelector(DOMStr.inputType).value,
+				desc: document.querySelector(DOMStr.inputDesc).value,
+				value: document.querySelector(DOMStr.inputValue).value,
+			};
+		},
+		getDOMStr: function () {
+			return DOMStr;
+		},
+	};
 })();
 
 // GLOBAL CONTROLLER
-let controller = (function (bdgtCtrl, uiCtrl) {
+let Controller = (function (bdgtCtrl, uiCtrl) {
+	let setupEventListeners = function () {
+		let DOMStr = uiCtrl.getDOMStr();
+		document
+			.querySelector(DOMStr.inputBtn)
+			.addEventListener("click", addItem);
+		document.addEventListener("keypress", function (event) {
+			if (event.keyCode === 13) {
+				addItem();
+			}
+		});
+	};
 
-    let addItem = function () {
-        
-    }
+	let addItem = function () {
+		let input = uiCtrl.getInput();
+	};
 
-    document.querySelector('.add__btn').addEventListener('click', addItem);
-    
-    document.addEventListener('keypress', function (event) {
+	return {
+		init: function () {
+			setupEventListeners();
+		},
+	};
+})(BudgetController, UIController);
 
-        if (event.keyCode === 13) {
-            
-            addItem();
-
-        }
-        
-    })
-    
-})(budgetController, uiController);
+Controller.init();
